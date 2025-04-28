52303389896634074eab8701c93065a8d49d9500
Dmitrii6776 <86263486+Dmitrii6776@users.noreply.github.com>
2025-04-28 22:15:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,9 +26,6 @@ def write_to_google_sheet(full_name, age, goal):
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
     client = gspread.authorize(creds)
-
-    client = gspread.authorize(creds)
-    
     sheet = client.open('Запись на тренировки').sheet1
     now = datetime.now().strftime("%d.%m.%Y %H:%M")
     data = [now, full_name, age, goal]
